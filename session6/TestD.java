0e227eb92e6f2b7626a71a74eff06e9a0475ce1a
Deepak Kumar <deepak18bcs@gmail.com>
2022-02-16 13:29:22 +0530

    session 8

---
+++ session6/TestD.java
@@ -0,0 +1,12 @@
+package session6;
+
+public class TestD {
+    public void thisIsPublic(){
+        System.out.println("hi pub");
+    }
+
+    protected
+     void thisIsProtected(){
+        System.out.println("hi proc");
+    }
+}
---
+++ session7/TestingMain.java
@@ -0,0 +1,13 @@
+package session7;
+import session6.TestD;
+
+public class TestingMain extends TestD {
+    public static void main(String[] args) {
+        
+        TestingMain tm = new TestingMain();
+
+        tm.thisIsPublic();
+        tm.thisIsProtected();
+
+    }
+}
