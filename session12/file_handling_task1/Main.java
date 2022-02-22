6ad1ebd9e28d893443b571891ac6ae3641880e2f
Deepak Kumar <deepak18bcs@gmail.com>
2022-02-22 16:53:19 +0530

    File Handling starter

---
+++ session12/file_handling_task1/Main.java
@@ -0,0 +1,35 @@
+package session12.file_handling_task1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    File sourceFile = new File("session12/file_handling_task1/inFile.txt");
+    File targetFile = new File("session12/file_handling_task1/outFile.txt");
+
+    copyContent(sourceFile, targetFile);
+  }
+
+  public static void copyContent(File a, File b) throws Exception {
+    FileInputStream in = new FileInputStream(a);
+    FileOutputStream out = new FileOutputStream(b);
+
+    try {
+      int n;
+      while ((n = in.read()) != -1) {
+        out.write(n);
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    } finally {
+      in.close();
+      out.close();
+    }
+
+    System.out.println("File Copied");
+    
+  }
+}
