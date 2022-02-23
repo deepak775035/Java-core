004b3247336391ecc34eed0bf2c9854af0384abd
Deepak Kumar <deepak18bcs@gmail.com>
2022-02-23 13:23:15 +0530

    session 13 need to cover up

--- session12/file_handling_task1/Main.java
+++ session12/file_handling_task1/Main.java
@@ -3,17 +3,18 @@ package session12.file_handling_task1;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws IOException {
     File sourceFile = new File("session12/file_handling_task1/inFile.txt");
     File targetFile = new File("session12/file_handling_task1/outFile.txt");
 
     copyContent(sourceFile, targetFile);
   }
 
-  public static void copyContent(File a, File b) throws Exception {
+  public static void copyContent(File a, File b) throws IOException  {
     FileInputStream in = new FileInputStream(a);
     FileOutputStream out = new FileOutputStream(b);
 
